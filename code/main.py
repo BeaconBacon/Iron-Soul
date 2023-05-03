2f8f18da74af6fdb6e295930586f0defff9947a4
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2023-05-02 23:51:29 -0400

    tons

--- code/main.py
+++ code/main.py
@@ -2,14 +2,13 @@ import pygame, sys
 from settings import *
 from level import Level
 
-
 class Game:
 	def __init__(self):
 		  
 		# general setup
 		pygame.init()
 		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Iron Soul')
+		pygame.display.set_caption('Zelda')
 		self.clock = pygame.time.Clock()
 
 		self.level = Level()
@@ -20,7 +19,7 @@ class Game:
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
-			
+
 			self.screen.fill('black')
 			self.level.run()
 			pygame.display.update()
@@ -28,4 +27,4 @@ class Game:
 
 if __name__ == '__main__':
 	game = Game()
-	game.run()	 
+	game.run()
